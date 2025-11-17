30a2068b1da6a0d200dbff283156d26183a94ac3
yj7370980-eng <yj7370980@gmail.com>
2025-11-18 00:51:51 +0530

    Change server port from 5000 to 10000

--- server.js
+++ server.js
@@ -38,5 +38,5 @@ app.get('/', (req, res) => {
   res.send('Inventory Management API');
 });
 
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 10000;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
